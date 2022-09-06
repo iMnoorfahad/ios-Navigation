334be70126c18cb2bf7f330b07b34d00896b3d8d
Noor Fahad <noor@drillmaps.com>
2022-09-06 17:08:58 +0530

    Infinite Navigation Screen

--- NavigationSwiftUI/ContentView.swift
+++ NavigationSwiftUI/ContentView.swift
@@ -9,8 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Screen(imageName: "house", title: "Welcome to Home", label: "Home", buttonName: "Home",buttonColor: .green)
+        }
+    }
+}
+
+struct Screen:View{
+    var imageName : String
+    var title : String
+    var label : String
+    var buttonName : String
+    var buttonColor: Color
+        
+    var body: some View{
+        
+            VStack{
+                Image(systemName: imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .padding()
+                Text(title).font(.system(size: 22, weight: .bold, design: .default))
+                    .padding()
+               NavigationLink(
+                destination: Screen(imageName: "bell", title: "Secont Screen", label: "Notification", buttonName: "Continue", buttonColor: .red),
+                    label:{
+                        CustomeButton(color: buttonColor,title: "Next")
+                    }
+                )
+            }
+            .navigationTitle(label)
+        }
+}
+
+struct CustomeButton:View {
+    var color: Color
+    var title: String
+    var body: some View{
+        Text(title)
+            .frame(width: 200, height: 50, alignment: .center)
+            .background(color)
+            .foregroundColor(.white)
+            .cornerRadius(10)
     }
 }
 
